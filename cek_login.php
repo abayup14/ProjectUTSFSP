8758e503399ea1d7dccccd900749f698fd6760ac
abayup14 <abayup1498@gmail.com>
2023-10-06 09:56:37 +0700

    Update cek_login.php

--- cek_login.php
+++ cek_login.php
@@ -3,28 +3,23 @@
     echo "<script src='js/jquery-3.7.0.js'></script>";
     require_once("class/users.php");
 
-    if (isset($_POST["txtusername"])) {
-        $iduser = $_POST["txtusername"];
-    } else {
-        $iduser = "";
-    }
-
-    if (isset($_POST["txtpassword"])) {
+    if (isset($_POST["btnLogin"])) {
+        $iduser = htmlentities(strip_tags($_POST["txtusername"]));
         $password = $_POST["txtpassword"];
-    } else {
-        $password = "";
+
+        $users = new Users();
+        $result = $users->getUser($iduser);
+
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            if (isset($_SESSION["iduser"])) {
+                $_SESSION["iduser"] = $user["iduser"];
+            } 
+            header("location: homepage.php");
+        } else {
+            header("location: index.php?code=false");
+        }
     }
 
-    $users = new Users();
-    $result = $users->getUser($iduser);
-
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
-        if (isset($_SESSION["iduser"])) {
-            $_SESSION["iduser"] = $user["iduser"];
-        } 
-        header("location: homepage.php");
-    } else {
-        header("location: index.php?code=false");
-    }
+    
 ?>
